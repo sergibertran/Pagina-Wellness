3538e345c9eb99b8fe9a3d019cf3e62076716eb8
alexrp202 <alexrp202@gmail.com>
2021-05-04 15:49:13 +0200

    componente calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 
 @NgModule({
@@ -53,7 +54,8 @@ import {MatSelectModule} from '@angular/material/select';
     AppComponent,
     AdminLayoutComponent,
     TipoIngredientesComponent,
-    HomeComponent
+    HomeComponent,
+    CalendarioComponent
     
     
     
