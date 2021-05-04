a97bb9ad674eb50a1581216948a22ce501bc94a8
alexrp202 <72034440+alexrp202@users.noreply.github.com>
2021-05-04 22:43:40 +0200

    Cambio de dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
 import { CalendarioComponent } from './calendario/calendario.component';
-import { NuevoEjercicioComponent } from './ejercicios/nuevo-ejercicio/nuevo-ejercicio.component';
-import { NuevoTipoEjercicioComponent } from './ejercicios/nuevo-tipo-ejercicio/nuevo-tipo-ejercicio.component';
-import { TipoIngredientesComponent } from './ingredientes/tipo-ingredientes/tipo-ingredientes.component';
-import { NuevoTipoDietaComponent } from './dieta/nuevo-tipo-dieta/nuevo-tipo-dieta.component';
-import { NuevoDietaComponent } from './dieta/nuevo-dieta/nuevo-dieta.component';
+
 
 FullCalendarModule.registerPlugins([ 
   dayGridPlugin,
@@ -71,13 +67,9 @@ FullCalendarModule.registerPlugins([
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    TipoIngredientesComponent,
     HomeComponent,
     CalendarioComponent,
-    NuevoEjercicioComponent,
-    NuevoTipoEjercicioComponent,
-    NuevoTipoDietaComponent,
-    NuevoDietaComponent
+   
     
     
     
