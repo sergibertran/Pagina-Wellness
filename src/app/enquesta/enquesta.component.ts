3ace58a0ae4eac564b9ec19dea024eab394c6450
mcanela30 <marticanela2015@gmail.com>
2021-05-06 19:05:19 +0200

    hacemos boton enquesta y cremos modelo

--- src/app/enquesta/enquesta.component.ts
+++ src/app/enquesta/enquesta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-enquesta',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnquestaComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-  }
-
+    this.registerForm = this.formBuilder.group({
+      nick: ['', Validators.required],
+      pwd: ['', [Validators.required, Validators.minLength(8)]],
+    });
 }
---
+++ src/app/models/enquesta.ts
@@ -0,0 +1,33 @@
+export class enquesta {
+    Nombre: string;
+    Apellidos: string;
+    Sexo: string;
+    Email: string; 
+    Edad: string;
+    Antecedentes: string;
+    FamBio: string; 
+    Peso: string;
+    Altura: string;
+    Cintura: string;
+    Cadera: string;
+    Intolerancia: string;
+    IntoleranciaEx: string;
+    Dieta: string;
+    DietaEx: string;
+    Lacteos: string;
+    Huevos: string;
+    Fruta: string;
+    Legumbres: string;
+    Carne: string;
+    CarneProcesada: string;
+    Pescado: string;
+    Bolleria: string;
+    AlimentosProcesados: string;
+    ComidaRapida: string;
+    BebidasAzucarada: string;
+    BebidasAlcoholicas: string;
+    AlimentoExeso: string;
+
+  }
+  
+  
