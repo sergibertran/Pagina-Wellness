1c718b024133fa9cc9652403aebbdd386a0062ae
mcanela30 <marticanela2015@gmail.com>
2021-05-06 16:37:23 +0200

    1

---
+++ src/app/enquesta/enquesta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enquesta',
+  templateUrl: './enquesta.component.html',
+  styleUrls: ['./enquesta.component.css']
+})
+export class EnquestaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
