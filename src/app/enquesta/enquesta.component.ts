b7fab39b2b4fbb879b44b8396a08497d09ca7e13
mcanela30 <marticanela2015@gmail.com>
2021-05-06 19:50:11 +0200

    sd

--- src/app/enquesta/enquesta.component.ts
+++ src/app/enquesta/enquesta.component.ts
@@ -20,8 +20,48 @@ export class EnquestaComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-
-      
+      Nombre: ['', Validators.required],
+      Apellidos: ['', Validators.required],
+      Sexo: ['', Validators.required],
+      Email: ['', Validators.required],
+      Edad: ['', Validators.required],
+      Antecedentes: ['', Validators.required],
+      FamBio: ['', Validators.required],
+      Peso: ['', Validators.required],
+      Altura: ['', Validators.required],
+      Cintura: ['', Validators.required],
+      Cadera: ['', Validators.required],
+      Intolerancia: ['', Validators.required],
+      IntoleranciaEx: ['', Validators.required],
+      Dieta: ['', Validators.required],
+      Lacteos: ['', Validators.required],
+      Huevos: ['', Validators.required],
+      Fruta: ['', Validators.required],
+      Legumbres: ['', Validators.required],
+      Carne: ['', Validators.required],
+      CarneProcesada: ['', Validators.required],
+      Pescado: ['', Validators.required],
+      Bolleria: ['', Validators.required],
+      AlimentosProcesados: ['', Validators.required],
+      ComidaRapida: ['', Validators.required],
+      BebidasAzucarada: ['', Validators.required],
+      BebidasAlcoholicas: ['', Validators.required],
+      AlimentoExeso: ['', Validators.required],      
     });
 }
+
+get f() { return this.registerForm.controls; }
+
+onSubmit() {
+  this.submitted = true;
+
+  // stop here if form is invalid
+  if (this.registerForm.invalid) {
+      return;
+  }
+
+
+}
+
+
 }
