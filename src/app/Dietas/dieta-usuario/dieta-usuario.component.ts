b34760a3f3ddff8b7b793bcb97d13af86efa25a8
mcanela30 <marticanela2015@gmail.com>
2021-05-12 19:39:42 +0200

    intento de dieta en user

--- src/app/Dietas/dieta-usuario/dieta-usuario.component.ts
+++ src/app/Dietas/dieta-usuario/dieta-usuario.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dieta-usuario',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dieta-usuario.component.css']
 })
 export class DietaUsuarioComponent implements OnInit {
-
-  constructor() { }
+  iduser;
+  term: string;
+  constructor(private http: HttpClient,private authService: AuthService) { }
+  filterData;
 
   ngOnInit(): void {
+    // this.iduser=this.authService.getidUser();
+    // this.authService.loadUsers(this.iduser)
+    // .pipe(first())
+    // .subscribe(
+    //   (data) => {
+    //      this.filterData=data;
+    //   console.log(data);      
+    // });
+  }
+
+  userClick(){
+console.log("test");
+
   }
 
 }
