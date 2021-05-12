d1cce4b8fb9708eaca0cdfad41c8d7f69694fe9d
mcanela30 <60703913+mcanela30@users.noreply.github.com>
2021-05-12 23:03:48 +0200

    AddRutina

--- src/app/Rutinas/add-rutina/add-rutina.component.ts
+++ src/app/Rutinas/add-rutina/add-rutina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-rutina',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-rutina.component.css']
 })
 export class AddRutinaComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+  nombreRutina: string;
+  tipoRutina: string;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      nombreRutina: ["", Validators.required],
+      tipoRutina: ["", Validators.required],
+    });
   }
 
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    
+    this.submitted = true;
+  
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+  
+    // display form values on success
+  
+      
+  }
 }
