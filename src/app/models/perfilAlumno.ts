0cfd398688fde2c06abaf241a845bf844e93ca56
sergibertran <bigres24@gmail.com>
2021-05-13 17:53:44 +0200

    bbb

---
+++ src/app/models/perfilAlumno.ts
@@ -0,0 +1,10 @@
+export class perfilAlumno {
+    constructor(
+      public usuario: string,
+      public nombre: string,
+      public apellidos: string,
+      public correo: string,
+      public direccion: string,
+      ){}
+    }
+    
