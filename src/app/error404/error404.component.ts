c66b8f1c67cce8db5ba619fb247240e7baf3742d
alexrp202 <72034440+alexrp202@users.noreply.github.com>
2021-05-16 23:57:43 +0200

    algunas validaciones y traducciones

--- src/app/error404/error404.component.ts
+++ src/app/error404/error404.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'app/services/auth.service';
+import { IdiomaService } from 'app/services/idioma.service';
 
 @Component({
   selector: 'app-error404',
@@ -9,9 +11,15 @@ import { AuthService } from 'app/services/auth.service';
 })
 export class Error404Component implements OnInit {
 
-  constructor( private router: Router,private authService: AuthService) { }
+  constructor( private _servicio:IdiomaService,public translate:TranslateService,private router: Router,private authService: AuthService) { }
 
   ngOnInit(): void {
+    if(this._servicio.getIdioma()==undefined){
+      this.translate.use(this.translate.getBrowserLang())
+      this._servicio.setIdioma(this.translate.getBrowserLang())
+  }else{
+      this.translate.use(this._servicio.getIdioma())
+  }
   }
 
   logOut(){
