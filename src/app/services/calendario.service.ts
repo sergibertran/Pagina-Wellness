af6fc12f7c8c3fa222efe249cced8b00599588bf
sergibertran <bigres24@gmail.com>
2021-05-17 18:35:14 +0200

    asdas

--- src/app/services/calendario.service.ts
+++ src/app/services/calendario.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class CalendarioService {
 
   load(text) {
    
-    
-    return this.http.post('https://wellnessdaw2.herokuapp.com/load.php/', JSON.stringify(text))
+
+    return this.http.post(`${environment.apiUrl}/load.php/`, JSON.stringify(text))
       .pipe(map(any => {
    
         
